f9eb4fcce3e3ba15e24595a1b893d8d72a8e0397
violeta ciaffoni <violeciaffoni04@gmail.com>
2025-11-08 12:34:49 -0300

    guardando los cambios de react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
-
+import { BrowserRouter, Routes, Route } from "react-router";
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+      </Routes>
       <NavBar />
       <ItemListContainer mensaje="¡Bienvenidos a Malvi-Accesorios!" />
-    </div>
+    </BrowserRouter>
   );
 }
 
