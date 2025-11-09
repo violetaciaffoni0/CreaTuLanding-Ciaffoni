f9e4898bdc27c2f5e4cd84c50c065a519e541996
violeta ciaffoni <violeciaffoni04@gmail.com>
2025-11-08 22:52:07 -0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 // main.jsx
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"; // Importar Bootstrap globalmente
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
