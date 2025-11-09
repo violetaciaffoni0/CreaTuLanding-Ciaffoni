eaccc62e0318d6cdca5cef1c453dd9a8eeb53355
violeta ciaffoni <violeciaffoni04@gmail.com>
2025-11-08 21:20:08 -0300

    corregi los import

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
-import { BrowserRouter, Routes, Route } from "react-router";
 import ItemDetailContainer from "./components/ItemDetailContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // ✅ CORREGIDO
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
+
       <Routes>
-        <Route path="/" element={<ItemListContainer />} />
+        <Route
+          path="/"
+          element={
+            <ItemListContainer mensaje="¡Bienvenidos a Malvi-Accesorios!" />
+          }
+        />
         <Route path="/category/:categoryId" element={<ItemListContainer />} />
         <Route path="/item/:itemId" element={<ItemDetailContainer />} />
         <Route path="*" element={<h2>404 - Página no encontrada</h2>} />
       </Routes>
-      <NavBar />
-      <ItemListContainer mensaje="¡Bienvenidos a Malvi-Accesorios!" />
     </BrowserRouter>
   );
 }
