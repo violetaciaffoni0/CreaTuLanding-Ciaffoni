b9daf79054aaca0c097e1aebce729fb6e56cea09
violeta ciaffoni <violeciaffoni04@gmail.com>
2025-11-09 02:40:20 -0300

    item

---
+++ src/components/Item.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+
+function Item({ item }) {
+  return (
+    <Col xs={12} sm={6} md={4} lg={3}>
+      <Card>
+        <Card.Img variant="top" src={item.imagen} />
+        <Card.Body>
+          <Card.Title>{item.nombre}</Card.Title>
+          <Card.Text>{item.descripcion}</Card.Text>
+          <p>
+            <strong>${item.precio}</strong>
+          </p>
+          <Link to={`/item/${item.id}`}>
+            <Button variant="primary">Ver más</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
+
+export default Item;
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,6 +4,7 @@ import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Item from "./Item";
 
 function ItemList({ items, mensaje }) {
   return (
@@ -14,21 +15,7 @@ function ItemList({ items, mensaje }) {
 
       <Row style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
         {items.map((item) => (
-          <Col key={item.id} xs={12} sm={6} md={4} lg={3}>
-            <Card>
-              <Card.Img variant="top" src={item.imagen} />
-              <Card.Body>
-                <Card.Title>{item.nombre}</Card.Title>
-                <Card.Text>{item.descripcion}</Card.Text>
-                <p>
-                  <strong>${item.precio}</strong>
-                </p>
-                <Link to={`/item/${item.id}`}>
-                  <Button variant="primary">Ver más</Button>
-                </Link>
-              </Card.Body>
-            </Card>
-          </Col>
+          <Item key={item.id} item={item} />
         ))}
       </Row>
     </Container>
