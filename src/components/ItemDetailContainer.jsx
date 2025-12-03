d90b3ea77dbb776cbf4faff0dc8d8aa66f2fe960
violeta ciaffoni <violeciaffoni04@gmail.com>
2025-12-02 21:17:58 -0300

    corregi item

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -2,16 +2,34 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetail from "./itemDetail";
 import { getProductById } from "../data/products";
+import { useCart } from "../context/useCart"; // IMPORT DEL HOOK
 
 function ItemDetailContainer() {
   const { id } = useParams();
   const [item, setItem] = useState(null);
+  const { addToCart } = useCart(); // OBTENER FUNCION PARA AGREGAR AL CARRITO
 
   useEffect(() => {
     getProductById(id).then((res) => setItem(res));
   }, [id]);
 
-  return <div>{item && <ItemDetail item={item} />}</div>;
+  const handleAddToCart = () => {
+    if (item) {
+      addToCart({ ...item, count: 1 }); // AGREGAMOS count = 1 POR DEFAULT
+      alert(`${item.title} agregado al carrito`); // OPCIONAL: feedback visual
+    }
+  };
+
+  return (
+    <div>
+      {item && (
+        <>
+          <ItemDetail item={item} />
+          <button onClick={handleAddToCart}>Agregar al carrito</button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default ItemDetailContainer;
